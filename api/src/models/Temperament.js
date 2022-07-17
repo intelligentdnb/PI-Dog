b1b40a6f3299469bde09054fe6606e8a62fcb758
intelligentdnb <juandiarts@gmail.com>
2022-07-17 05:16:44 -0300

    Base de datos

---
+++ api/src/models/Temperament.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+
+//Exporto la funcion temperamento
+module.exports = sequelize => {
+    //defino temperament
+    sequelize.define('temperament', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+};
